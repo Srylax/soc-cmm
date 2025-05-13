6b9985c3e6d477d9d2ef1237ad1cd1010497b495
Srylax <71783705+Srylax@users.noreply.github.com>
2025-05-13 20:17:00 +0200

    refactor: Move Answer into its own module

--- cmm-core/src/lib.rs
+++ cmm-core/src/lib.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
 
+use answer::Answer;
 use strum::FromRepr;
+
+mod answer;
 
 pub enum Domain {
     People,
@@ -8,102 +11,6 @@ pub enum Domain {
     Process,
     Technology,
     Services,
-}
-
-#[derive(Clone, Copy, Debug, FromRepr)]
-pub enum Satisfaction {
-    No = 1,
-    Somewhat = 2,
-    Averagely = 3,
-    Mostly = 4,
-    Fully = 5,
-}
-impl Default for Satisfaction {
-    fn default() -> Self {
-        Self::No
-    }
-}
-
-#[derive(Clone, Copy, Debug, FromRepr)]
-pub enum Occurence {
-    Never = 1,
-    Sometimes = 2,
-    Averagely = 3,
-    Mostly = 4,
-    Always = 5,
-}
-impl Default for Occurence {
-    fn default() -> Self {
-        Self::Never
-    }
-}
-
-#[derive(Clone, Copy, Debug, FromRepr)]
-pub enum Detailed {
-    No = 1,
-    Partially = 2,
-    Averagely = 3,
-    Mostly = 4,
-    Fully = 5,
-}
-impl Default for Detailed {
-    fn default() -> Self {
-        Self::No
-    }
-}
-
-#[derive(Clone, Copy, Debug, FromRepr)]
-pub enum DetailedOptional {
-    No = 1,
-    Partially = 2,
-    Averagely = 3,
-    Mostly = 4,
-    Fully = 5,
-    NotRequired = 6,
-}
-impl Default for DetailedOptional {
-    fn default() -> Self {
-        Self::No
-    }
-}
-
-#[derive(Debug)]
-pub enum Answer {
-    Satisfaction(Satisfaction),
-    Detailed(Detailed),
-    DetailedOptional(DetailedOptional),
-    Occurence(Occurence),
-    Bool(bool),
-    Any(String),
-    None,
-}
-
-impl Answer {
-    pub fn in_scope(&self) -> bool {
-        matches!(
-            self,
-            Answer::Satisfaction(_)
-                | Answer::Occurence(_)
-                | Answer::Detailed(_)
-                | Answer::DetailedOptional(_)
-        )
-    }
-    pub fn score(&self) -> Option<u8> {
-        match self {
-            Answer::Satisfaction(satisfaction) => Some(*satisfaction as u8),
-            Answer::Occurence(occurence) => Some(*occurence as u8),
-            Answer::Detailed(detailed) => Some(*detailed as u8),
-            Answer::DetailedOptional(detailed_optional) => Some(*detailed_optional as u8),
-            _ => None,
-        }
-    }
-    pub fn max_score(&self) -> Option<u8> {
-        match self {
-            Answer::Satisfaction(_) | Answer::Occurence(_) | Answer::Detailed(_) => Some(5),
-            Answer::DetailedOptional(_) => Some(6),
-            _ => None,
-        }
-    }
 }
 
 pub struct Aspect {
