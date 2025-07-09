11e2947b879ca9e5f4bb979df90561f69f3ca256
Timo Borer <timoborer@gmail.com>
2025-07-09 14:03:47 +0200

    feat: Update overview component style

--- cmm-web/src/components/overview.rs
+++ cmm-web/src/components/overview.rs
@@ -1,7 +1,8 @@
-use crate::{components::ProgressBarComponent, components::BadToGoodProgressBarComponent, utils::round};
+use crate::{components::{BadToGoodProgressBarComponent, DomainIconComponent}, utils::round};
 use cmm_core::{CMM, Domain};
 use dioxus::prelude::*;
 use strum::VariantArray;
+use dioxus_free_icons::{icons::fa_solid_icons::FaStar, Icon};
 
 #[component]
 pub fn OverviewComponent() -> Element {
@@ -29,7 +30,7 @@ pub fn OverviewComponent() -> Element {
             },
         },
         div {
-            class: "w-full max-w-5xl mx-auto mt-8 mb-16",
+            class: "w-full max-w-3xl mx-auto mt-8 mb-16",
             id: "domain-scores",
             h2 {
                 class: "text-3xl mb-4",
@@ -53,43 +54,71 @@ fn DomainOverviewComponent(domain: Domain) -> Element {
 
     rsx! {
         div {
-            class: "w-full bg-slate-100 p-4 rounded",
+            class: "w-full bg-slate-100 p-4 rounded-2xl border-slate-300 border-1 dark:border-slate-700 dark:bg-slate-800",
             div {
-                class: "flex justify-between items-center mb-2 w-full",
-                h2 {
-                    class: "text-2xl",
-                    "{domain}"
-                },
+                class: "bg-slate-200 border-1 border-slate-300 mb-2 p-4 rounded-2xl dark:border-slate-700 dark:bg-slate-900",
                 div {
-                    class: "text-xl",
-                    title: "{overall_score} / 5",
-                    "{round(overall_score / 5.0 * 100.0, 0)}%",
-                }
-            },
-            BadToGoodProgressBarComponent {
-                value: overall_score,
-                height: 2,
-                max: 5.0
+                    class: "flex justify-between items-center w-full",
+                    div {
+                        class: "flex items-center gap-2",
+                        div {
+                            class: "bg-blue-500 rounded-xl p-3 aspect-square",
+                            DomainIconComponent {
+                                width: 20,
+                                height: 20,
+                                fill: "white",
+                                domain
+                            }
+                        },
+                        h2 {
+                            class: "text-2xl font-semibold",
+                            "{domain}"
+                        },
+                    }
+                    div {
+                        class: "text-xl text-center grid",
+                        title: "{overall_score} / 5",
+                        small {
+                            class: "text-xs",
+                            "Maturity"
+                        }
+                        "{round(overall_score / 5.0 * 100.0, 0)}%",
+                    }
+                },
+                // div {
+                //     class: "grid items-center gap-2 mb-1 grid-cols-[6rem_1fr]",
+                //     span {
+                //         class: "text-sm",
+                //         "Total maturity"
+                //     },
+                //     BadToGoodProgressBarComponent {
+                //         value: overall_score,
+                //         height: 2,
+                //         max: 5.0
+                //     },
+                // },
             },
             div {
-                class: "mt-2",
+                class: "mt-4 bg-slate-50 rounded-2xl p-4 border-1 border-slate-200 dark:border-slate-500 dark:bg-slate-600",
                 for (i, aspect) in cmm.read().aspect(&domain).unwrap().iter().enumerate() {
                     div {
                         key: format!("{}{}_{}", aspect.title(), aspect.maturity_score(), aspect.capability_score()),
                         span {
-                            class: "text-xs",
+                            class: "text-[10px] text-right",
                             "{aspect.title()}"
                         },
-                        BadToGoodProgressBarComponent {
-                            max: 5.0,
-                            value: aspect.maturity_score()
-                        },
-                        if aspect.capability_score().is_normal() {
-                            div {
-                                class: "mt-1",
-                                BadToGoodProgressBarComponent {
-                                    max: 5.0,
-                                    value: aspect.capability_score(),
+                        div {
+                            BadToGoodProgressBarComponent {
+                                max: 5.0,
+                                value: aspect.maturity_score()
+                            },
+                            if aspect.capability_score().is_normal() {
+                                div {
+                                    class: "mt-1",
+                                    BadToGoodProgressBarComponent {
+                                        max: 5.0,
+                                        value: aspect.capability_score(),
+                                    }
                                 }
                             }
                         }
---
+++ cmm-web/src/components/ui/domain_icon.rs
@@ -0,0 +1,73 @@
+use cmm_core::Domain;
+use dioxus::prelude::*;
+use dioxus_free_icons::{
+    icons::fa_solid_icons::{
+        FaBriefcase,
+        FaUsers,
+        FaBarsProgress,
+        FaWrench,
+        FaComputer
+    }, Icon};
+
+
+
+#[component]
+pub fn DomainIconComponent(
+    domain: Domain,
+    width: u32,
+    height: u32,
+    fill: String
+) -> Element {
+    match domain {
+        Domain::Business => {
+            rsx!{
+                Icon {
+                    width,
+                    height,
+                    fill,
+                    icon: FaBriefcase
+                }
+            }
+        },
+        Domain::People => {
+            rsx!{
+                Icon {
+                    width,
+                    height,
+                    fill,
+                    icon: FaUsers
+                }
+            }
+        },
+        Domain::Process => {
+            rsx!{
+                Icon {
+                    width,
+                    height,
+                    fill,
+                    icon: FaBarsProgress
+                }
+            }
+        },
+        Domain::Services => {
+            rsx!{
+                Icon {
+                    width,
+                    height,
+                    fill,
+                    icon: FaWrench
+                }
+            }
+        },
+        Domain::Technology => {
+            rsx!{
+                Icon {
+                    width,
+                    height,
+                    fill,
+                    icon: FaComputer
+                }
+            }
+        },
+    }
+}
